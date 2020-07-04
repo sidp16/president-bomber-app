f460adb4f4f80e44bce80e312d9194206c150de8
Siddharth Prasad <siddharthp@Siddharths-MacBook-Pro.local>
2020-07-04 19:48:07 +0100

    finished characters page, functionality wise

---
+++ lib/roles_and_descriptions.dart
@@ -0,0 +1,31 @@
+const List roles = [
+  'Bomber',
+  'President',
+  'Blue Hostage',
+  'Red Hostage',
+  'Gambler',
+  'Sniper',
+  'Target',
+  'Decoy',
+  'Mastermind',
+  'Hot Potato',
+  'Hero',
+  'Anarchist',
+  'Traveler'
+];
+
+const List roleDescriptions = [
+  'Your allegiance is RED. Your aim is to kill the president by being in the same room as them at the end of the last round, and if you succeed, the red team wins.',
+  'Your allegiance is BLUE. Provided you are in a different room to the Bomber at the end of the last round, the blue team wins, as you are alive.',
+  'Your allegiance is BLUE. Your role is to aid the President in his bid to be in the opposite room as the Bomber at the end of the last round.',
+  'Your allegiance is RED. Your role is to aid the Bomber in his attempt to kill the president by being in the same room as them at the end of the last round.',
+  'You have no allegiance. Your role is to guess who will win at the end of the game after the last swap has been made, and if you are correct, you win along with the winning party',
+  'You have no allegiance. At the end of the last round, before all players reveal their character cards, you must publicly announce which player you are shooting.\n\nThe selected player does not have to be in the same room as you. You win if you kill the Target along with the winning party. Kill anyone else and you lose.',
+  'You have no allegiance. You win if the Sniper does not shoot you at the end of the last round.',
+  'You have no allegiance. You win if the Sniper shoots you at the end of the last round.',
+  'You have no allegiance. At the end of the last round, before all players reveal their cards, you, the Mastermind, must publicly announce the color of EVERY player.\n\nIf you are exactly right, you win and everyone else loses.',
+  'You have no allegiance. Any player that card shares or even color shares with you immediately trades cards with you.\n\nBoth you and the other player assume the powers and the allegiance of the newly acquired cards. At the end of the game, the Hot Potato loses.',
+  'You have no allegiance. You win if you are in the same room as the President and Bomber at the end of the game. If you win, the both Red and Blue teams lose.',
+  'Your have no allegiance. You win if you are able to gain leader status in a majority of the rounds. Hence, you would have to be leader 2 times in a 3 round game.',
+  "You have no allegiance. You win if you are sent to a different room as a hostage at the end of MOST rounds. Hence, you would have to be moved 2 times in a 3 round game.",
+];
--- lib/views/RulesCharactersScreen.dart
+++ lib/views/RulesCharactersScreen.dart
@@ -1,3 +1,161 @@
+//import 'package:flutter/cupertino.dart';
+//import 'package:flutter/material.dart';
+//import 'package:presidentbomber/drawers.dart';
+//
+//void main() {
+//  runApp(CharacterRulesScreen());
+//}
+//
+//class CharacterRulesScreen extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      debugShowCheckedModeBanner: false,
+//      title: 'Flutter Demo',
+//      theme: ThemeData(
+//        primarySwatch: Colors.blue,
+//        visualDensity: VisualDensity.adaptivePlatformDensity,
+//      ),
+//      home: MyHomePage(),
+//    );
+//  }
+//}
+//
+//class MyHomePage extends StatefulWidget {
+//  @override
+//  _MyHomePageState createState() => _MyHomePageState();
+//}
+//
+//class _MyHomePageState extends State<MyHomePage> {
+//  ScrollController controller = ScrollController();
+//  bool closeTopContainer = false;
+//  double topContainer = 0;
+//
+//  List<Widget> itemsData = [];
+//
+//  void getPostsData() {
+//    List<dynamic> responseList;
+//    List<Widget> listItems = [];
+//    responseList.forEach((post) {
+//      listItems.add(Container(
+//          height: 150,
+//          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+//          decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20.0)), color: Colors.white, boxShadow: [
+//            BoxShadow(color: Colors.black.withAlpha(100), blurRadius: 10.0),
+//          ]),
+//          child: Padding(
+//            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+//            child: Row(
+//              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//              children: <Widget>[
+//                Column(
+//                  crossAxisAlignment: CrossAxisAlignment.start,
+//                  children: <Widget>[
+//                    Text(
+//                      'Bomber',
+//                      style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+//                    ),
+//                    Text(
+//                      'Win Condition',
+//                      style: const TextStyle(fontSize: 17, color: Colors.grey),
+//                    ),
+//                    SizedBox(
+//                      height: 10,
+//                    ),
+//                    Text(
+//                      'Allegiance',
+//                      style: const TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold),
+//                    )
+//                  ],
+//                ),
+//                Image.asset(
+//                  "assets/images/${post["image"]}",
+//                  height: double.infinity,
+//                )
+//              ],
+//            ),
+//          )));
+//    });
+//    setState(() {
+//      itemsData = listItems;
+//    });
+//  }
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//    getPostsData();
+//    controller.addListener(() {
+//
+//      double value = controller.offset/119;
+//
+//      setState(() {
+//        topContainer = value;
+//        closeTopContainer = controller.offset > 50;
+//      });
+//    });
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    final Size size = MediaQuery.of(context).size;
+//    final double categoryHeight = size.height*0.30;
+//    return SafeArea(
+//      child: Scaffold(
+//        appBar: AppBar(title: Text("Characters & Rules")),
+//        drawer: HomeScreenDrawer(),
+//        backgroundColor: Colors.white,
+//        body: Container(
+//          height: size.height,
+//          child: Column(
+//            children: <Widget>[
+//              const SizedBox(
+//                height: 10,
+//              ),
+//              AnimatedOpacity(
+//                duration: const Duration(milliseconds: 200),
+//                opacity: closeTopContainer?0:1,
+//                child: AnimatedContainer(
+//                    duration: const Duration(milliseconds: 200),
+//                    width: size.width,
+//                    alignment: Alignment.topCenter),
+////                    height: closeTopContainer?0:categoryHeight
+//              ),
+//              Expanded(
+//                  child: ListView.builder(
+//                      controller: controller,
+//                      itemCount: itemsData.length,
+//                      physics: BouncingScrollPhysics(),
+//                      itemBuilder: (context, index) {
+//                        double scale = 1.0;
+//                        if (topContainer > 0.5) {
+//                          scale = index + 0.5 - topContainer;
+//                          if (scale < 0) {
+//                            scale = 0;
+//                          } else if (scale > 1) {
+//                            scale = 1;
+//                          }
+//                        }
+//                        return Opacity(
+//                          opacity: scale,
+//                          child: Transform(
+//                            transform:  Matrix4.identity()..scale(scale,scale),
+//                            alignment: Alignment.bottomCenter,
+//                            child: Align(
+//                                heightFactor: 0.7,
+//                                alignment: Alignment.topCenter,
+//                                child: itemsData[index]),
+//                          ),
+//                        );
+//                      })),
+//            ],
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
+
 // Flutter code sample for BottomNavigationBar
 
 // This example shows a [BottomNavigationBar] as it is used within a [Scaffold]
@@ -11,12 +169,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:presidentbomber/drawers.dart';
+import 'package:presidentbomber/roles_and_descriptions.dart';
 
 void main() => runApp(CharacterRulesScreen());
 
 /// This Widget is the main application widget.
 class CharacterRulesScreen extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'Characters and Rules';
 
   @override
   Widget build(BuildContext context) {
@@ -38,13 +197,44 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Rules',
-      style: optionStyle,
+  List<Widget> _widgetOptions = <Widget>[
+    Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+            child: ListView.builder(
+//              physics: BouncingScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: roles.length,
+                itemBuilder: (context, index) {
+                  return Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Card(
+                          child: ListTile(
+                            onTap: () {},
+                            title: Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: Text(roles[index],
+                                  style: TextStyle(fontSize: 20)),
+                            ),
+                            subtitle: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(roleDescriptions[index],
+                                  style: TextStyle(fontSize: 17)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }))
+      ],
     ),
     Text(
-      'Characters',
+      'Rules',
       style: optionStyle,
     ),
   ];
@@ -59,7 +249,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Rules & Characters'),
+        title: const Text('Characters and Rules'),
       ),
       drawer: CharacterRulesDrawer(),
       body: Center(
@@ -68,12 +258,12 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.library_books, size: 20),
-            title: Text('Rules'),
-          ),
-          BottomNavigationBarItem(
             icon: Icon(Icons.contacts, size: 20),
             title: Text('Characters'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books, size: 20),
+            title: Text('Rules'),
           ),
         ],
         currentIndex: _selectedIndex,
