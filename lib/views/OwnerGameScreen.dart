51c5f3b6e33de93345f0fbc529bef078a8b412de
Siddharth Prasad <siddharthp@Siddharths-MacBook-Pro.local>
2020-07-05 02:27:29 +0100

    temporary timer

--- lib/views/OwnerGameScreen.dart
+++ lib/views/OwnerGameScreen.dart
@@ -10,18 +10,41 @@ import 'package:presidentbomber/widgets/buttons.dart';
 import '../alert_dialogs.dart';
 import '../text_on_screen.dart';
 
-class OwnerGameScreen extends StatelessWidget {
+class OwnerGameScreen extends StatefulWidget {
   final String gameId;
   final String name;
-  Timer _timer;
 
   OwnerGameScreen(this.gameId, this.name);
+
+  @override
+  _OwnerGameScreenState createState() => _OwnerGameScreenState();
+}
+
+class _OwnerGameScreenState extends State<OwnerGameScreen> {
+  int _counter = 10;
+  Timer _timer;
+
+  void _startTimer() {
+    _counter = 10;
+    if (_timer != null) {
+      _timer.cancel();
+    }
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_counter > 0) {
+          _counter--;
+        } else {
+          _timer.cancel();
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("$gameId | Owner Console"),
+        title: Text("${widget.gameId} | Owner Console"),
         flexibleSpace: Container(
           decoration: BoxDecoration(
               gradient: LinearGradient(
@@ -34,7 +57,7 @@ class OwnerGameScreen extends StatelessWidget {
       body: StreamBuilder(
         stream: Firestore.instance
             .collection(COLLECTION_NAME)
-            .document(this.gameId)
+            .document(this.widget.gameId)
             .snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData)
@@ -47,19 +70,19 @@ class OwnerGameScreen extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    BlueHostageButton(
-                        gameId, snapshot.data[ROLES], snapshot.data[PLAYERS]),
+                    BlueHostageButton(widget.gameId, snapshot.data[ROLES],
+                        snapshot.data[PLAYERS]),
                     Container(
                       height: 103,
                       child: DistributeButton(
-                          gameId,
-                          name,
+                          widget.gameId,
+                          widget.name,
                           snapshot.data[ROLES],
                           snapshot.data[PLAYERS],
-                          snapshot.data[DISTRIBUTIONS][name]),
+                          snapshot.data[DISTRIBUTIONS][widget.name]),
                     ),
-                    RedHostageButton(
-                        gameId, snapshot.data[ROLES], snapshot.data[PLAYERS]),
+                    RedHostageButton(widget.gameId, snapshot.data[ROLES],
+                        snapshot.data[PLAYERS]),
                   ],
                 ),
               ),
@@ -68,9 +91,9 @@ class OwnerGameScreen extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    SniperButton(gameId: gameId),
-                    GamblerButton(gameId: gameId),
-                    MastermindButton(gameId: gameId),
+                    SniperButton(gameId: widget.gameId),
+                    GamblerButton(gameId: widget.gameId),
+                    MastermindButton(gameId: widget.gameId),
                   ],
                 ),
               ),
@@ -79,9 +102,9 @@ class OwnerGameScreen extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    TargetButton(gameId: gameId),
-                    HeroButton(gameId: gameId),
-                    DecoyButton(gameId: gameId),
+                    TargetButton(gameId: widget.gameId),
+                    HeroButton(gameId: widget.gameId),
+                    DecoyButton(gameId: widget.gameId),
                   ],
                 ),
               ),
@@ -90,9 +113,9 @@ class OwnerGameScreen extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    HotPotatoButton(gameId: gameId),
-                    AnarchistButton(gameId: gameId),
-                    TravelerButton(gameId: gameId),
+                    HotPotatoButton(gameId: widget.gameId),
+                    AnarchistButton(gameId: widget.gameId),
+                    TravelerButton(gameId: widget.gameId),
                   ],
                 ),
               ),
@@ -101,16 +124,36 @@ class OwnerGameScreen extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    ClearRolesButton(gameId: gameId),
-                    LeaveGameButton(gameId, name),
+                    ClearRolesButton(gameId: widget.gameId),
+                    LeaveGameButton(widget.gameId, widget.name),
+                    RaisedButton(
+                      onPressed: () => _startTimer(),
+                      child: Text("Start"),
+                    ),
                   ],
                 ),
               ),
               RolesLobbyMessage(snapshot.data[PLAYERS], snapshot.data[ROLES]),
               PlayersListMessage(snapshot.data[PLAYERS]),
               RolesListMessage(snapshot.data[ROLES]),
-              UniqueRoleMessage(snapshot.data[DISTRIBUTIONS][name], name),
-          ],
+              UniqueRoleMessage(
+                  snapshot.data[DISTRIBUTIONS][widget.name], widget.name),
+              (_counter > 0)
+                  ? Text("")
+                  : Text(
+                      "DONE!",
+                      style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+              Text('$_counter',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  )),
+            ],
           );
         },
       ),
